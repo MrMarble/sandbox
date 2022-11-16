b841491972106b537f3a997d28ae31cea96df312
Alvaro Tinoco <alvarotinocomarmol@gmail.com>
2022-11-16 18:13:11 +0100

    feat: add brush control

--- cell.go
+++ cell.go
@@ -22,13 +22,20 @@ func NewCell(cType CellType) *Cell {
 	clr := color.RGBA{0, 0, 0, 0}
 	switch cType {
 	case SAND:
-		clr = color.RGBA{0xb2, 0xc2, 0x80, 255}
+		clr = color.RGBA{0xc2, 0xb2, 0x80, 255}
 	case WATER:
-		clr = color.RGBA{0x00, 0x00, 0xff, 255}
+		clr = color.RGBA{0x09, 0xc3, 0xdb, 255}
 	case WALL:
 		clr = color.RGBA{0x10, 0x10, 0x10, 255}
 	case STONE:
 		clr = color.RGBA{0x80, 0x80, 0x80, 255}
 	}
 	return &Cell{cType: cType, color: clr}
+}
+
+func (c *Cell) WithVariation(variation byte) *Cell {
+	c.color.R += variation
+	c.color.G += variation
+	c.color.B += variation
+	return c
 }
--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"log"
+	"math"
 	"math/rand"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"golang.org/x/exp/constraints"
 )
 
 func init() {
@@ -16,8 +19,8 @@ func init() {
 }
 
 const (
-	screenWidth  = 320
-	screenHeight = 240
+	screenWidth  = 640
+	screenHeight = 480
 )
 
 type Game struct {
@@ -25,12 +28,30 @@ type Game struct {
 	sanbox *Sandbox
 
 	selectedCellType CellType
+	brushSize        int
+
+	prevPos   [2]int
+	cursorPos [2]int
+
+	cellQueue [][2][2]int
+}
+
+func (g *Game) updateCursor() {
+	g.prevPos = g.cursorPos
+	x, y := ebiten.CursorPosition()
+	g.cursorPos = [2]int{x, y}
 }
 
 func (g *Game) HandleInput() {
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		cursorX, cursorY := ebiten.CursorPosition()
-		g.sanbox.SetCell(cursorX, cursorY, *NewCell(g.selectedCellType))
+		g.cellQueue = append(g.cellQueue, [2][2]int{g.prevPos, g.cursorPos})
+	}
+
+	_, scrollY := ebiten.Wheel()
+	if scrollY > 0 {
+		g.brushSize = min(50, g.brushSize+2)
+	} else if scrollY < 0 {
+		g.brushSize = max(2, g.brushSize-2)
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.Key1) {
@@ -41,11 +62,33 @@ func (g *Game) HandleInput() {
 		g.selectedCellType = WALL
 	} else if inpututil.IsKeyJustPressed(ebiten.Key4) {
 		g.selectedCellType = STONE
+	} else if inpututil.IsKeyJustPressed(ebiten.Key5) {
+		g.selectedCellType = EMPTY
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		g.sanbox = NewSandbox(screenWidth, screenHeight)
 	}
 }
 
+func max[T constraints.Ordered](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func (g *Game) Update() error {
+	g.updateCursor()
 	g.HandleInput()
+	g.placeParticles()
+
 	g.sanbox.Update()
 	return nil
 }
@@ -57,8 +100,11 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	}
 
 	g.sanbox.Draw(g.pixels)
-
 	screen.WritePixels(g.pixels)
+	//TODO: Change to custom path
+	curX, curY := ebiten.CursorPosition()
+	ebitenutil.DrawRect(screen, float64(curX-g.brushSize/2), float64(curY-g.brushSize/2), float64(g.brushSize), float64(g.brushSize), color.White)
+	// DEBUG
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f\nType: %d", ebiten.ActualTPS(), ebiten.ActualFPS(), g.selectedCellType))
 }
 
@@ -70,11 +116,61 @@ func main() {
 	g := &Game{
 		sanbox:           NewSandbox(screenWidth, screenHeight),
 		selectedCellType: SAND,
+		brushSize:        10,
 	}
 
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
 	ebiten.SetWindowTitle("Sandbox")
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func (g *Game) placeParticles() {
+	for {
+		if len(g.cellQueue) == 0 {
+			break
+		}
+
+		p1, p2 := g.cellQueue[0][0], g.cellQueue[0][1]
+		g.cellQueue = g.cellQueue[1:]
+		p1x, p1y := p1[0], p1[1]
+		p2x, p2y := p2[0], p2[1]
+
+		dx := int(math.Abs(float64(p2x - p1x)))
+		sx := -1
+		if p1x < p2x {
+			sx = 1
+		}
+		dy := -int(math.Abs(float64(p2y - p1y)))
+		sy := -1
+		if p1y < p2y {
+			sy = 1
+		}
+		err := dx + dy
+		for {
+			for x := p1x - (g.brushSize / 2); x < (p1x + g.brushSize/2); x++ {
+				for y := p1y - (g.brushSize / 2); y < (p1y + g.brushSize/2); y++ {
+					if x >= 0 && x < screenWidth && y >= 0 && y < screenHeight {
+						if g.selectedCellType == EMPTY || g.sanbox.IsEmpty(x, y) {
+							variable := byte(rand.Intn(20))
+							g.sanbox.SetCell(x, y, *NewCell(g.selectedCellType).WithVariation(variable))
+						}
+					}
+				}
+			}
+			if p1x == p2x && p1y == p2y {
+				break
+			}
+			e2 := 2 * err
+			if e2 >= dy {
+				err += dy
+				p1x += sx
+			}
+			if e2 <= dx {
+				err += dx
+				p1y += sy
+			}
+		}
 	}
 }
